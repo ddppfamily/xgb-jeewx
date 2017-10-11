75488b6e285f9b62a53d74707078acfa392d76fe
ddppfamily <ddppfamily@gmail.com>
2017-10-11 17:57:19 +0800

    master

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 cd jeecg-p3-biz-alipay-single
 mvn install package -Dmaven.test.skip=true
 
-cd ..\jeecg-p3-biz-qywx
+cd ../jeecg-p3-biz-qywx
 mvn install package -Dmaven.test.skip=true
 
-cd ..\jeewx
-set MAVEN_OPTS=%MAVEN_OPTS% -Xms1024M -Xmx1024M -XX:PermSize=256M -XX:MaxPermSize=256M
+cd ../jeewx
+MAVEN_OPTS=%MAVEN_OPTS% -Xms1024M -Xmx1024M -XX:PermSize=256M -XX:MaxPermSize=256M
 mvn tomcat:run
 
 echo 'build project successfully!'
