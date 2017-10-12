a67940463df82a197e733ccfc1984643a19616ae
ddppfamily <ddppfamily@gmail.com>
2017-10-12 22:30:02 +0800

    master

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-cd jeecg-p3-biz-alipay-single
-mvn install package -Dmaven.test.skip=true
-
-cd ../jeecg-p3-biz-qywx
-mvn install package -Dmaven.test.skip=true
-
-cd ../jeewx
-MAVEN_OPTS=%MAVEN_OPTS% -Xms1024M -Xmx1024M -XX:PermSize=256M -XX:MaxPermSize=256M
-mvn tomcat:run
-
-echo 'build project successfully!'
+nohup sh install.sh > nohup.log &
